d1ae5024244772f95d71bd73031b13c890f81de3
Kyoya Yamaguchi <kyoryu32@gmail.com>
2024-11-24 15:12:19 +0900

    Initial Commit

---
+++ FreeHand/FreeHandApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeHandApp.swift
+//  FreeHand
+//
+//  Created by Kyoya Yamaguchi on 2024/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FreeHandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
